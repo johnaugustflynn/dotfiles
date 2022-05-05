3753c088daa105063399f72fa74b9a6bbd424d67
John Flynn <jaflynn23@gmail.com>
2022-05-05 12:08:39 -0400

    nvim lsp

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -44,15 +44,21 @@ return packer.startup(function(use)
     use {"catppuccin/nvim", as = "catppuccin"}
 
     -- cmp
-    use "hrsh7th/nvim-cmp"             -- the completion plugin
-    use "hrsh7th/cmp-buffer"           -- buffer completions
-    use "hrsh7th/cmp-path"             -- path completions
-    use "hrsh7th/cmp-cmdline"          -- cmdline completions
-    use "saadparwaiz1/cmp_luasnip"     -- snippet completions
+    use "hrsh7th/nvim-cmp"                -- the completion plugin
+    use "hrsh7th/cmp-buffer"              -- buffer completions
+    use "hrsh7th/cmp-path"                -- path completions
+    use "hrsh7th/cmp-cmdline"             -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip"        -- snippet completions
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-nvim-lua"
 
     -- snippets
-    use "L3MON4D3/LuaSnip"             -- snippet engine
-    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+    use "L3MON4D3/LuaSnip"                -- snippet engine
+    use "rafamadriz/friendly-snippets"    -- a bunch of snippets to use
+
+    -- LSP
+    use "neovim/nvim-lspconfig"           -- enable LSP
+    use "williamboman/nvim-lsp-installer" -- simple to use language server nvim-lsp-installer
 
     if PACKER_BOOTSTRAP then
         require('packer').sync()
