7626c63f6b021ff1eb8d98f9c6872721a0e9e8cc
John Flynn <jaflynn23@gmail.com>
2022-07-14 21:11:38 -0400

    spaceship prommpt

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -1,2 +1,5 @@
-# export PS1=''
+autoload -U promptinit; promptinit
+prompt spaceship
+
+export PROMPT_COMMAND='if [ -d .git -a ! -x .git/hooks/pre-commit -a -e .pre-commit-config.yaml ] && which pre-commit >& /dev/null; then pre-commit install; fi;'
 
--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 echo 'export ZDOTDIR=~/.config/zsh' > ~/.zshenv
+mkdir -p /usr/local/share/zsh/site-functions/
 
 [ ! -d ~/.config ] && mkdir ~/.config
 
 ln -sf "$PWD/config/zsh" ~/.config/  
 ln -sf "$PWD/config/nvim" ~/.config/
+
