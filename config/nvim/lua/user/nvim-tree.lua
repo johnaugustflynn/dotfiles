3b75ce57c713f53ad3cc50722bee3f8ede379a54
John Flynn <jaflynn23@gmail.com>
2022-07-17 13:37:54 -0400

    nvim tree mappings

--- config/nvim/lua/user/nvim-tree.lua
+++ config/nvim/lua/user/nvim-tree.lua
@@ -3,5 +3,19 @@ if not status_ok then
     return
 end
 
-nvim_tree.setup()
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+  return
+end
+
+nvim_tree.setup {
+    view = {
+        mappings = {
+            list = {
+                { key = "u", action = "dir_up" },
+                { key = "v", cb = nvim_tree_config.nvim_tree_callback "vsplit" },
+            }
+        }
+    }
+}
 
