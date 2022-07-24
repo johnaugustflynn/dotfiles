b95a1aafe6ea21069933fd019a144947c023c550
John Flynn <jaflynn23@gmail.com>
2022-07-24 10:35:59 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+# A reference list of commands to remember to run when setting up a macbook. 
+# Definitely not exhaustive because I dont remember everything I did.
+
+echo 'ERROR: This script is not meant to be ran directly.'
+exit 1
+
+defaults write -g InitialKeyRepeat -int 10 # default minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1         # default minimum is 2 (30 ms)
 
 echo 'export ZDOTDIR=~/.config/zsh' > ~/.zshenv
 mkdir -p /usr/local/share/zsh/site-functions/
 
 [ ! -d ~/.config ] && mkdir ~/.config
 
-ln -sf "$PWD/config/zsh" ~/.config/  
+ln -sf "$PWD/config/zsh" ~/.config/ 
 ln -sf "$PWD/config/nvim" ~/.config/
 
