453b1cd17af1245ff5b0de293ad16dabaa92a7ca
John Flynn <jaflynn23@gmail.com>
2022-07-24 17:02:34 -0400

    simplify nvim-tree require

--- config/nvim/lua/user/nvim-tree.lua
+++ config/nvim/lua/user/nvim-tree.lua
@@ -3,17 +3,12 @@ if not status_ok then
     return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
 nvim_tree.setup {
     view = {
         mappings = {
             list = {
                 { key = "u", action = "dir_up" },
-                { key = "v", cb = nvim_tree_config.nvim_tree_callback "vsplit" },
+                { key = "v", cb = require("nvim-tree.config").nvim_tree_callback "vsplit" },
             }
         }
     }
