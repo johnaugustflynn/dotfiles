c914c0015df4d94ae2e62b6bd6c8225494f334f6
John Flynn <jaflynn23@gmail.com>
2022-07-31 10:11:25 -0400

    gopls

--- config/nvim/lua/user/lsp/lsp-installer.lua
+++ config/nvim/lua/user/lsp/lsp-installer.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "clangd", "pyright" ,"rust_analyzer" }
+local servers = { "jsonls", "sumneko_lua", "clangd", "pyright", "rust_analyzer", "gopls" }
 
 lsp_installer.setup {
     ensure_installed = servers
