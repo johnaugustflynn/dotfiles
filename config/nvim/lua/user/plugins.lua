51abb9775ceebcfc0d662610458eb88d25210d51
John Flynn <john.flynn.codes@gmail.com>
2023-01-27 11:54:55 -0500

    vim-fugitive

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -90,6 +90,7 @@ return packer.startup(function(use)
     use "nvim-telescope/telescope-media-files.nvim"
 
     -- git
+    use "tpope/vim-fugitive"
     use "lewis6991/gitsigns.nvim"
 
     -- terminal
