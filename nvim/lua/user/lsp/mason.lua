fb8e8241c2ba3a44be70c55fb1b214cc40cabb32
John Flynn <john.flynn.codes@gmail.com>
2023-02-06 17:17:29 -0500

    fix mason.lua

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -1,53 +1,68 @@
+local mason_ok, mason = pcall(require, "mason")
+local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
+local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+
+if not (mason_ok and lspconfig_ok and mason_lspconfig_ok) then
+    return
+end
+
 local servers = {
+    "bashls",
     "clangd",
+    "cssls",
     "gopls",
+    "html",
     "jsonls",
     "pyright",
     "rust_analyzer",
     "sumneko_lua",
+    "tsserver",
 }
 
-require("mason").setup()
-require("mason-lspconfig").setup({
+mason.setup()
+mason_lspconfig.setup({
     ensure_installed = servers,
     automatic_installation = true,
 })
 
-local lspconfig = require("lspconfig")
+for _, server in pairs(servers) do
+    local opts = {
+        on_attach = require("user.lsp.handlers").on_attach,
+        capabilities = require("user.lsp.handlers").capabilities,
+    }
 
-lspconfig.sumneko_lua.setup({
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.stdpath("config") .. "/lua"] = true,
+    if server == "sumneko_lua" then
+        opts.settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { "vim" },
+                },
+                workspace = {
+                    library = {
+                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                        [vim.fn.stdpath("config") .. "/lua"] = true,
+                    },
                 },
             },
-        },
-    },
-})
+        }
+    end
 
-lspconfig.jsonls.setup({
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-    settings = {
-        json = {
-            schemas = require("user.lsp.jsonschemas"),
-        },
-    },
-    setup = {
-        commands = {
-            Format = {
-                function()
-                    vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
-                end,
+    if server == "jsonls" then
+        opts.settings = {
+            json = {
+                schemas = require("user.lsp.jsonschemas"),
             },
-        },
-    },
-})
+        }
+        opts.setup = {
+            commands = {
+                Format = {
+                    function()
+                        vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
+                    end,
+                },
+            },
+        }
+    end
+
+    lspconfig[server].setup(opts)
+end
